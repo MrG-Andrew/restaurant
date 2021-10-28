9e3ff402c0e298139ce6bc4e12a1d9db9beb0a04
andrew <alm3allmandy@gmail.com>
2021-10-28 09:01:12 +0200

    added user login and register

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
-Route::get('/home',[HomeController::class,'index']);
+Route::get('/',[HomeController::class,'index']);
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
